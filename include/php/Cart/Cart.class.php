f438d262c7753daffb7e4b87ed736c78debbe4c7
cpuwhiz11 <paul_geromini@student.uml.edu>
2013-04-19 14:55:54 -0400

    Update cart class to be in line with DB and coding style

--- include/php/Cart/Cart.class.php
+++ include/php/Cart/Cart.class.php
@@ -7,39 +7,39 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/Database/Database.class.p
 class Cart {
 
 	/* Add a single book to a user's cart */
-	public static function addToCart($userId, $bookId){
+	public static function AddToCart($user_id, $book_id){
 	
-		$query = "INSERT INTO Cart
-		          VALUES ('" . $userId . "' , '" . $bookId . "')";
+		$query = "INSERT INTO shopping_cart
+		          VALUES (?, ?)", "is", $user_id, $book_id; 
 				  
 		return Database::Query($query);
 	
 	}
 	
 	/* Remove an existing item from a user's cart */
-	public static function removeFromCart($userId, $bookId){
+	public static function RemoveFromCart($user_id, $book_id){
 	
-		$query = "DELETE FROM Cart
-		          WHERE userId ='" . $userId . "'" . " AND bookId ='" . $bookId . "'"; 
+		$query = "DELETE FROM shopping_cart
+		          WHERE user_id = ? AND book_id = ?", "is",  $user_id, $book_id; 
 				  
 		return Database::Query($query);
 	}
 	
 	/* Get all the items in a user's cart */
-	public static function getCart($userId){
+	public static function GetCart($user_id){
 	
 		$query = "SELECT *
-		          FROM Cart
-		          WHERE userId= '" . $userId . "'"; 
+		          FROM shopping_cart
+				  WHERE user_id = ?", "i", $user_id;
 				  
 		return Database::Query($query);
 	}
 	
-	/* Remove all exisiting items in a user's cart */
-	public static function emptyCart($userId){
+	/* Remove all exisiting items in a user's shopping_cart */
+	public static function EmptyCart($user_id){
 	
-		$query = "DELETE FROM Cart
-		          WHERE userId ='" . $userId ."'"; 
+		$query = "DELETE FROM shopping_cart
+		          WHERE user_id = ?", "i", $user_id;
 				  
 		return Database::Query($query);
 	
