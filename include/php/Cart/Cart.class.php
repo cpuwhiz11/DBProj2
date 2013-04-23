d4ced164729396a94e40fa0accc1ef648bfcbd8b
cpuwhiz11 <paul_geromini@student.uml.edu>
2013-04-23 18:41:00 -0400

    Fixed wrong syntax. Added in function to update quantity in cart. Return more pricing info via Cart.GetCart().

--- include/php/Cart/Cart.class.php
+++ include/php/Cart/Cart.class.php
@@ -7,12 +7,12 @@ require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/Database/Database.class.p
 class Cart {
 
 	/* Add a single book to a user's cart */
-	public static function AddToCart($user_id, $book_id){
+	public static function AddToCart($user_id, $book_id, $quantity){
 	
 		$query = "INSERT INTO shopping_cart
-		          VALUES (?, ?)", "is", $user_id, $book_id; 
+		          VALUES (?, ?, ?)"; 
 				  
-		return Database::Query($query);
+		return Database::Query($query, "isi", $user_id, $book_id, $quantity);
 	
 	}
 	
@@ -20,28 +20,43 @@ class Cart {
 	public static function RemoveFromCart($user_id, $book_id){
 	
 		$query = "DELETE FROM shopping_cart
-		          WHERE user_id = ? AND book_id = ?", "is",  $user_id, $book_id; 
+		          WHERE user_id = ? AND book_id = ?";
 				  
-		return Database::Query($query);
+		return Database::Query($query, "is",  $user_id, $book_id);
+	}
+	
+	/* Update the quantity of a particular book the user is ordering */
+	public static function UpdateQuantity($user_id, $book_id, $quantity){
+	
+		$query = "UPDATE shopping_cart
+		          SET quantity = ?
+				  WHERE user_id = ? AND book_id = ?";
+				  
+		return Database::Query($query, "iis", $quantity, $user_id, $book_id; );
 	}
 	
 	/* Get all the items in a user's cart */
 	public static function GetCart($user_id){
-	
-		$query = "SELECT *
-		          FROM shopping_cart
-				  WHERE user_id = ?", "i", $user_id;
+		
+		$query = "SELECT shopping_cart.user_id,
+		                 shopping_cart.book_id,
+						 books.price, books.title,
+						 books.price * shopping_cart.quantity AS total
+                  FROM shopping_cart
+                  INNER JOIN books
+                  ON shopping_cart.book_id = books.isbn
+                  WHERE shopping_cart.user_id = ?";
 				  
-		return Database::Query($query);
+		return Database::Query($query, "i", $user_id);
 	}
 	
 	/* Remove all exisiting items in a user's shopping_cart */
 	public static function EmptyCart($user_id){
 	
 		$query = "DELETE FROM shopping_cart
-		          WHERE user_id = ?", "i", $user_id;
+		          WHERE user_id = ?";
 				  
-		return Database::Query($query);
+		return Database::Query($query, "i", $user_id);
 	
 	}
 
