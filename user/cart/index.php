0cfce9bda3175b34ec5c7cc4382eefac16f9df9e
Ryan Field <mark_field@student.uml.edu>
2013-04-25 12:04:04 -0400

    Set up cart page

--- user/cart/index.php
+++ user/cart/index.php
@@ -1,7 +1,76 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php 
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/Auth/Auth.class.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/Cart/Cart.class.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/User/User.class.php");
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/include/php/Search/Search.class.php");
+	
+	if(!Auth::IsLoggedIn()) { header("location: /auth/login/"); }
+	
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link href="/include/css/default_1.css" rel="stylesheet" type="text/css"/>
+		<script src="/include/js/jQuery.js"></script>
+        <title>Shopping Cart</title>
+		<script>
+			
+			$(document).ready(function() {
+				
+				$("#search_field").focus();
+				
+			});
+			
+		</script>
+    </head>
+    <body>
+		
+		<nav>
+			<div id="logo">
+				<img src="/include/img/booksrus_logo_250.png"/>
+			</div>
+			
+			<div id="search">
+				<form action="search/" method="get">
+					<div id="seach_in_select">
+						<select name="c">
+							<option value="title">Title</option>
+							<option value="author">Author</option>
+							<option value="publishers">Publishers</option>
+							<option value="keyword">Keyword</option>
+						</select>
+					</div>
+					<div id="search_box">
+						<input type="text" name="s" id="search_field" />
+					</div>
+					<div id="submit_button">
+						<input type="submit" value="Go"/>
+					</div>
+				</form>
+			</div>
+			
+			<div id="user">
+				Hello, <a href="/user/"><?php echo $_SESSION["username"]; ?></a>.
+				<a href="/user/cart/" class="shopping_cart_link">Cart (<?php echo Cart::GetNumberItemsInCart($_SESSION["user_id"]); ?>)</a>
+				<a href="/auth/logout/" class="logout_link"></a>
+			</div>
+						
+		</nav>
+		
+		<div id="content">
+			
+			<?php 
+			
+				$cart = Cart::GetCart($_SESSION["user_id"]);
+				
+				echo "<pre>";
+				print_r($cart);
+				echo "</pre>";
+			
+			?>
+			
+			
+		</div>
+		
+</html>
