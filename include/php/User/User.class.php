321493e86444f0b046c640bcb3e22859e49f8ada
Paul Geromini <pgeromini@crossbeam.com>
2013-04-25 11:07:16 -0400

    Fix syntax errors

--- include/php/User/User.class.php
+++ include/php/User/User.class.php
@@ -9,34 +9,36 @@ class User {
 	/* Get the books a user has ordered */
 	public static function MakeOrder($user_id, $shipping_address,
 	                                   $total_price, $tax, $shipping){
-		// Create order entry						   
-		$query = "INSERT INTO orders (date, user_id, shipping_address, total_price, tax, shipping)
-		          VALUES (NOW(), ?, ?, ?, ?, ?)"; 
+            // Create order entry						   
+            $query = "INSERT INTO orders (date, user_id, shipping_address, total_price, tax, shipping)
+            	          VALUES (NOW(), ?, ?, ?, ?, ?)"; 
 				  
-	    Database::Query($query, "isiii", $user_id, $shipping_address, $total_price,
-		                                 $tax, $shipping);
+            Database::Query($query, "isiii", $user_id, $shipping_address, $total_price,
+                                             $tax, $shipping);
 										 
-		$query = "SELECT LAST_INSERT_ID()
-		          FROM orders";
+            $query = "SELECT LAST_INSERT_ID()
+                      FROM orders";
 									
-        // Get the id of the order we just made									
-		$order_id = Database::Query($query)[0]["LAST_INSERT_ID()"];
+            // Get the id of the order we just made									
+            $order_id = Database::Query($query);
+                        
+            $order_id = $order_id[0]["LAST_INSERT_ID()"];
 		
-		// Get all the book isbns and quantities the user has in cart
-		$cartArray = Cart::GetCart($user_id);
+            // Get all the book isbns and quantities the user has in cart
+            $cartArray = Cart::GetCart($user_id);
 		
-		// Save the books the user bought in orders
-		// For each book run a seperate query
-		foreach($cartArray as $book) {
-			$query = "INSERT INTO order_items (order_id, isbn, quantity)
-					VALUES (?, ?, ?)";
+            // Save the books the user bought in orders
+            // For each book run a seperate query
+            foreach($cartArray as $book) {
+                    $query = "INSERT INTO order_items (order_id, isbn, quantity)
+                              VALUES (?, ?, ?)";
 				  
-			Database::Query($query, "isi" $order_id, $book["book_id"], $book["quantity"]);
-		}
+                    Database::Query($query, "isi", $order_id, $book["book_id"], $book["quantity"]);
+            }
 		
-		// Delete cart for this user
-	    return Cart::EmptyCart($user_id)
-	}
+            // Delete cart for this user
+            return Cart::EmptyCart($user_id);
+        }
 	
 	
 	/* Get the orders user has placed */
@@ -45,18 +47,18 @@ class User {
 		$query = "SELECT user_id, shipping_address, total_price,
 		                 tax, shipping
 		          FROM orders
-				  WHERE user_id = ?"; 
+			  WHERE user_id = ?"; 
 				  
 	    return Database::Query($query, "i", $user_id);
 		
 	}
 	
 	/* Get the books a user made on a order */
-	public static function GetOrderHistory($order_id ){
+	public static function GetOrderBooks($order_id ){
 						   
 		$query = "SELECT isbn, quantity
 		          FROM order_items
-				  WHERE order_id = ?";
+			  WHERE order_id = ?";
 				 
 	    return Database::Query($query, "i", $order_id );
 		
